e322af58c93344f1cb72696b03fb3fa16e171a93
Julian Grisales <jgrisales@gmail.com>
2020-09-25 14:34:47 +0200

    @Repository

--- src/main/java/com/pincelab/market/persistence/ProductoRepository.java
+++ src/main/java/com/pincelab/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.pincelab.market.persistence;
 
 import com.pincelab.market.persistence.crud.ProductoCrudRepository;
 import com.pincelab.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +22,21 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void deleteById(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+    public void deleteProduct(Producto producto){
+        productoCrudRepository.delete(producto);
     }
 }
