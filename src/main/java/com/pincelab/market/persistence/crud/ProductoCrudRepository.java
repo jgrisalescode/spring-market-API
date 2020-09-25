d15dd5b26c7763fa07468bb8cf7f33194b88ee10
Julian Grisales <jgrisales@gmail.com>
2020-09-25 14:29:37 +0200

    QueryMethods

--- src/main/java/com/pincelab/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/pincelab/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,16 @@ package com.pincelab.market.persistence.crud;
 import com.pincelab.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    /*
+        Using QueryMethods see all in:
+        https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+     */
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 
 }
