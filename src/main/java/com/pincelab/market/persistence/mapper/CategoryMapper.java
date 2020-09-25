70bd7f81b34094952bdfd8187e3172db53a494fc
Julian Grisales <jgrisales@gmail.com>
2020-09-25 15:27:41 +0200

    Repositories to Domain

---
+++ src/main/java/com/pincelab/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.pincelab.market.persistence.mapper;
+
+import com.pincelab.market.domain.Category;
+import com.pincelab.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true) // Ignore because in Category is not there
+    Categoria toCategoria(Category category);
+}
